0da0a38b20c16f32a531f24ad47ee0cfc83f8a99
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2016-11-17 18:02:39 +0900

    1st apdate

---
+++ src/test/jp/ac/uryukyu/ie/e165746/EnemyTest.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e165746;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165747 on 2016/11/17.
+ */
+public class EnemyTest {
+    @Test
+    public void attack() throws Exception {
+
+    }
+
+}
