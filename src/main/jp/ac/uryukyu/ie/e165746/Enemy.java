c4ccfe152876eb865afc4a3f83ce532babfeef0d
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2016-11-18 17:36:12 +0900

    2nd update Enemy.java

--- src/main/jp/ac/uryukyu/ie/e165746/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165746/Enemy.java
@@ -1,7 +1,7 @@
 package jp.ac.uryukyu.ie.e165746;
 
 /**
- * Created by e165747 on 2016/11/17.
+ * arranged by e165747 on 2016/11/17.
  */
 public class Enemy {
 
@@ -48,12 +48,13 @@ public class Enemy {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param hero 攻撃対象
      */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+    public void attack(Hero hero) {
+        if (!dead) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }
     }
-
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
