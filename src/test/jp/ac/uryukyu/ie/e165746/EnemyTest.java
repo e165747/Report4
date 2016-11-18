e3b56aa64b4bbc87a49a845e6da1d99b0ae34d94
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2016-11-18 17:19:47 +0900

    1st apdate EnemyTest

--- src/test/jp/ac/uryukyu/ie/e165746/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165746/EnemyTest.java
@@ -4,12 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by e165747 on 2016/11/17.
- */
 public class EnemyTest {
     @Test
     public void attack() throws Exception {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.dead = true;
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.hitPoint);
 
     }
 
